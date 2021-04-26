96c4a79147c8a94358b1eea9ad65e51e4515111b
supawadee khamnuengsithi <35362594+supawadee0789@users.noreply.github.com>
2021-04-26 15:54:48 +0700

    add all of project page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Projects from "./components/Project";
 import ProjectComponent from "./components/ProjComponent";
 
 import img1 from "./img/good-wallet.png";
+import img2 from "./img/mtm.png";
+import img3 from "./img/browser.svg";
 function App() {
   return (
     <div className="App">
@@ -25,8 +27,17 @@ function App() {
       <Activities/>
       <Dott/>
       <Projects/>
-      <ProjectComponent bgColor="#E0EEF9" img={img1} textColor="#003D6B" projTitle="Project" projContent="isus"/>
+      <ProjectComponent bgColor="#E0EEF9" img={img1} textColor="#003D6B" projTitle="Flutter" projContent="My 4th years student project or Senior project is moblie application that was written by flutter framework.
+       It's a expensed management application which can record transactions by voice for decrese time to take note and comfortable to make income statement. "
+       responsibility="My responsibilities are designing application using Adobe XD, Developing front-end and handling database by using firebase cloud firestore."
+       github="https://github.com/supawadee0789/goodwallet_app" />
+       <ProjectComponent bgColor="#003D6B" img={img2} textColor="#E0EEF9" projTitle="React Native" projContent="Macth the Memories is application to note English words with auto translation in Thai. And making random flashcards to users who learn English and want to remember vocabulary faster."
+       responsibility="Design and Develop whole application." github="https://github.com/supawadee0789/Match-the-Memory"/>
+       <ProjectComponent bgColor="#E0EEF9" img={img3} textColor="#003D6B" projTitle="React" projContent="This website was written by HTML,CSS,JavaScript and React.This website was created for learning to create website and using new library. Including a detailed description of my background and personal information."
+       responsibility="Design and Develop whole website."
+       github="https://github.com/supawadee0789/Supawadee_Resume" />
     </div>
+
   );
 }
 
--- src/components/ProjComponent.js
+++ src/components/ProjComponent.js
@@ -1,18 +1,65 @@
-import React from 'react';
+import React from "react";
 
-function ProjectComponent(props){
-    return(
-        <div style={{height: "100vh", backgroundColor:props.bgColor ,flexDirection:'row', alignItems:'center',justifyContent:'space-around',display:'flex'}}>
-        <img src={props.img} width="30%"/>
-        <div style={{flexDirection:'column', alignItems:'start',justifyContent:'center',display:'flex',width:'35%'}}>
-           <h1 style={{fontWeight:'bold',color:props.textColor,marginBottom:50}}>{props.projTitle}</h1> 
-           <p style={{fontSize:'1rem',color:props.textColor,letterSpacing: 1,textAlign:'start'}}>{props.projContent}
-           </p>
-                
-        </div>
-        
+function ProjectComponent(props) {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        backgroundColor: props.bgColor,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        display: "flex",
+      }}
+    >
+      <img src={props.img} width="40%" style={{ marginRight: "5%" }} />
+      <div
+        style={{
+          flexDirection: "column",
+          alignItems: "start",
+          justifyContent: "start",
+          display: "flex",
+          width: "35%",
+        }}
+      >
+        <h1
+          style={{
+            fontWeight: "bold",
+            color: props.textColor,
+            marginBottom: 50,
+          }}
+        >
+          {props.projTitle}
+        </h1>
+        <p
+          style={{
+            fontSize: "1rem",
+            color: props.textColor,
+            letterSpacing: 0.5,
+            textAlign: "start",
+          }}
+        >
+          {props.projContent}
+        </p>
+        <br />
+        <p
+          style={{
+            fontSize: "0.85rem",
+            color: props.textColor,
+            letterSpacing: 0.5,
+            textAlign: "start",
+          }}
+        >
+          <span style={{ fontWeight: "bold", color: props.textColor,letterSpacing: 0.5, textAlign: "start",}}>
+            Responsibility :{" "}
+          </span>
+          {props.responsibility}
+        </p>
+        <br/>
+        <p style={{ fontWeight: "bold", color: props.textColor,fontSize:'0.85rem',textAlign: "start", }}>See on github repositories : <a href={props.github} style={{fontWeight:'normal' ,color:props.textColor}}>{props.github}</a></p>
+      </div>
     </div>
-    );
+  );
 }
 
 export default ProjectComponent;
